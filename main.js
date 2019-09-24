0944929b5eeed9cf314b41e41bf7d4f6b5514e73
jjarey <48677977+jjarey@users.noreply.github.com>
2019-09-24 11:29:48 +0300

    Added show details buttons to the cards

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 //  Document ready
 $(function() {
+  // Event handler for hamburger menu
   $('.mobile-nav-icon').click(function() {
-     // Hide and show navigation
-     $('.nav').toggle();
-     // Mobile menu icon animaton
-     $('.bar1').toggleClass('change1');
-     $('.bar2').toggleClass('change2');
-     $('.bar3').toggleClass('change3');
-   });
+    // Hide and show navigation on click
+    $('.nav').toggle();
+    // Mobile menu icon animaton
+    $('.bar1').toggleClass('change1');
+    $('.bar2').toggleClass('change2');
+    $('.bar3').toggleClass('change3');
+  });
+  // Event handler for details buttons
+  $('.details-button').click(function() {
+    // Show details on click
+    $(this).next('.details').toggle();
+  });
 });
